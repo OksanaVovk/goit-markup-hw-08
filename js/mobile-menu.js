c3228ba9608a7d2e57afa672cb4c55bbcc822fa6
OksanaVovk <oksvovk85@gmail.com>
2022-02-16 23:18:55 +0200

    Поправки до роботи

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -9,6 +9,9 @@
         menuBtnRef.classList.toggle("is-open");
         // menuBtnRef.setAttribute("aria-expanded"), !expanded);
     
-    mobileMenuRef.classList.toggle("is-open");
-});
+        mobileMenuRef.classList.toggle("is-open");
+        
+   
+    });
+
 })();
